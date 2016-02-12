796d15646bc557f42d011208aab6878af692415e
sx <sunxin0630@126.com>
2016-02-12 20:02:03 +0800

    试验提交手机卫士

--- app/src/main/java/com/sx/phoneguard/activities/HomeActivity.java
+++ app/src/main/java/com/sx/phoneguard/activities/HomeActivity.java
@@ -10,7 +10,7 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        initView();
+        initView();//初始化界面
     }
 
     private void initView() {
