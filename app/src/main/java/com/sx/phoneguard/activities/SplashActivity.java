0835d301116882388e552fed8578750ec73b4824
sx <sunxin0630@126.com>
2016-02-13 00:09:55 +0800

    第二次提交安全卫士，Splash界面上，添加了下载功能，下载还有进度条显示，不过有bug，总是下载失败

--- app/src/main/java/com/sx/phoneguard/activities/SplashActivity.java
+++ app/src/main/java/com/sx/phoneguard/activities/SplashActivity.java
@@ -1,17 +1,27 @@
 package com.sx.phoneguard.activities;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
 import android.os.SystemClock;
+import android.view.View;
 import android.view.animation.AlphaAnimation;
+import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.lidroid.xutils.HttpUtils;
+import com.lidroid.xutils.exception.HttpException;
+import com.lidroid.xutils.http.ResponseInfo;
+import com.lidroid.xutils.http.callback.RequestCallBack;
 import com.sx.phoneguard.R;
 import com.sx.phoneguard.domain.UrlData;
 import com.sx.phoneguard.utils.ShowToast;
@@ -20,6 +30,7 @@ import com.sx.phoneguard.utils.Stream2String;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
@@ -38,6 +49,7 @@ public class SplashActivity extends Activity {
     private PackageInfo packageInfo;
     private RelativeLayout rl_root;
     private int versionCode;
+    private ProgressBar pb_download;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,9 +59,10 @@ public class SplashActivity extends Activity {
         initAnimation();//初始化界面的动画效果
         startSubVersion();//访问网络监测版本信息
     }
+
     //访问网络检测版本信息是个耗时操作，所以要开一个子线程进行
-    public void startSubVersion(){
-        new Thread(){
+    public void startSubVersion() {
+        new Thread() {
             @Override
             public void run() {
                 checkVersion();
@@ -57,13 +70,54 @@ public class SplashActivity extends Activity {
         }.start();
     }
 
-    Handler handler = new Handler(){
+   Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
-            switch (msg.what){
+            switch (msg.what) {
                 case UPDATEVERSION:
+                    //弹出对话框提示有新版本，并提供下载新版本的Url
+                    //获取新版本的信息
+                    final UrlData data = (UrlData) msg.obj;
+                    //弹出对话框
+                    AlertDialog.Builder builder = new AlertDialog.Builder(SplashActivity.this);
+
+                    /**
+                     * 如果用户不更新也不取消，而是点击了返回键
+                     *      1,另返回键无效
+                     *      2，设置返回事件
+                     */
+                    //点返回键无效
+                    //                    builder.setCancelable(false);
+                    //设置取消返回事件
+                    builder.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                        @Override
+                        public void onCancel(DialogInterface dialog) {
+                            //直接加载主界面
+                            loadMain();
+                        }
+                    });
+
+                    builder.setTitle("监测到新版本！").setMessage(data.getDesc())
+                            .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    //直接跳入主界面
+                                    loadMain();
+                                }
+                            })
+                            .setPositiveButton("更新", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    //获取Url下载新版本,使用xUtils下载
+                                    downAndInstall(data.getDownloadUrl());
+
+                                }
+                            });
+                        builder.show();
+
                     break;
                 case LOADMAIN:
+                    //加载主界面
                     loadMain();
                     break;
 
@@ -71,7 +125,79 @@ public class SplashActivity extends Activity {
                     break;
             }
         }
+
+        /**
+         * 下载，安装更新。
+         * @param url
+         */
+        private void downAndInstall(String url) {
+            HttpUtils httpUtils = new HttpUtils();
+            //开始下载的时候显示进度条
+            pb_download.setVisibility(View.VISIBLE);
+            httpUtils.download(url, "/sdcard/phoneguard.apk", true, true, new RequestCallBack<File>() {
+                /*
+                下载成功
+                 */
+                @Override
+                public void onSuccess(ResponseInfo<File> responseInfo) {
+                    //开始下载apk，要跳转到下载apk的界面。需要查看源码中的实现下载的Activity是哪个。packages\apps\PackageInstaller
+                    /*
+
+                    <intent - filter >
+                        <action android:name = "android.intent.action.VIEW" / >
+                        <category android:name = "android.intent.category.DEFAULT" / >
+                        <data android:scheme = "content" / >
+                        <data android:scheme = "file" / >
+                        <data android:mimeType = "application/vnd.android.package-archive" / >
+                    </intent - filter >
+
+                    */
+                    //就是要启动上面的那个.PackageInstallerActivity
+                    //下载成功之后，开始安装之前，让进度条消失
+                    pb_download.setVisibility(View.GONE);
+                    Intent intent = new Intent();
+                    intent.setAction("android.intent.action.VIEW");
+                    intent.addCategory("android.intent.category.DEFAULT");
+                    intent.setDataAndType(Uri.fromFile(new File(Environment.getExternalStorageDirectory(),"phoneguard.apk")),
+                            "application/vnd.android.package-archive");
+                    //开启PackageInstallerActivity
+                    //如果在这个界面用户不点击安装，而是选择了不安装，会返回来一个消息。
+                    startActivityForResult(intent,0);
+                }
+
+
+                /*
+                    设置进度条
+                 */
+                @Override
+                public void onLoading(long total, long current, boolean isUploading) {
+                    super.onLoading(total, current, isUploading);
+                    pb_download.setProgress((int) current);
+                    pb_download.setMax((int) total);
+                }
+
+                @Override
+                public void onFailure(HttpException e, String s) {
+                    ShowToast.show(SplashActivity.this, "下载失败!");
+                    loadMain();
+                }
+            });
+        }
+
+
     };
+
+    /**
+     * 当用户在安装界面的时候选择了不安装，返回数据，直接跳转到主界面
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        loadMain();
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 
     /**
      * 访问网络，监测版本信息。URL地址需要配置成静态的，在Values文件夹下进行配置
@@ -91,7 +217,7 @@ public class SplashActivity extends Activity {
             conn.setReadTimeout(5000);
             //拿到响应码
             int code = conn.getResponseCode();
-            if(code==200){
+            if (code == 200) {
                 //请求成功
                 //拿到服务器返回的输入流，也就是json数据，转换成字符串信息
                 InputStream is = conn.getInputStream();
@@ -100,45 +226,47 @@ public class SplashActivity extends Activity {
                 //解析json数据,
                 UrlData urlData = ParseJson(text);
                 //处理数据，判断版本号是否一致，如果不一致，说明有新版本
-                if(versionCode != urlData.getVersionCode()){
+                if (versionCode != urlData.getVersionCode()) {
                     //使用对话框显示是否更新新版本
-                    msg.what=UPDATEVERSION;
+                    msg.what = UPDATEVERSION;
+                    //发送消息，携带信息
+                    msg.obj = urlData;
 
-                }else {
+                } else {
                     //没有新版本，直接进入主界面,执行后面finally中的进入主界面
 
                 }
-            }else {
+            } else {
                 //2000 请求失败，直接进入主界面
-                ShowToast.show(SplashActivity.this,"2000 请求失败");
+                ShowToast.show(SplashActivity.this, "2000 请求失败");
 
             }
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
             //2001 Url错误，直接进入主界面
-            ShowToast.show(SplashActivity.this,"2001 Url错误");
+            ShowToast.show(SplashActivity.this, "2001 Url错误");
         } catch (IOException e) {
             e.printStackTrace();
             //2002 IO错误，网络超时，直接进入主界面
-            ShowToast.show(SplashActivity.this,"2002 IO错误，网络超时");
+            ShowToast.show(SplashActivity.this, "2002 IO错误，网络超时");
         } catch (JSONException e) {
             e.printStackTrace();
             //2003 json数据格式错误，直接进入主界面
-            ShowToast.show(SplashActivity.this,"2003 json数据格式错误");
-        }finally {
-            if(msg.what==UPDATEVERSION){
+            ShowToast.show(SplashActivity.this, "2003 json数据格式错误");
+        } finally {
+            if (msg.what == UPDATEVERSION) {
                 //什么都不做
-            }else {
+            } else {
                 msg.what = LOADMAIN;
             }
             long endTime = System.currentTimeMillis();//耗时操作的结束时间
             //耗时操作的时间 = endTime - startTime
             //判断时间如果小于2秒，就休眠两秒，如果大于两秒，那么就该几秒几秒
-            if((endTime-startTime)<2000){
+            if ((endTime - startTime) < 2000) {
                 //休眠
-                SystemClock.setCurrentTimeMillis(2000-(endTime-startTime));
-            }else {
+                SystemClock.setCurrentTimeMillis(2000 - (endTime - startTime));
+            } else {
                 //休眠时间超过2秒。不管了。
             }
             msg.sendToTarget();
@@ -148,14 +276,15 @@ public class SplashActivity extends Activity {
     /**
      * 加载进入主界面
      */
-    public void loadMain(){
-        Intent intent = new Intent(SplashActivity.this,HomeActivity.class);
+    public void loadMain() {
+        Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
         startActivity(intent);
         finish();//关闭Splash界面
     }
 
     /**
      * 解析json数据
+     *
      * @param text
      */
     private UrlData ParseJson(String text) throws JSONException {
@@ -173,7 +302,7 @@ public class SplashActivity extends Activity {
      */
     private void initAnimation() {
         //透明渐变动画效果
-        AlphaAnimation aa = new AlphaAnimation(0,1);
+        AlphaAnimation aa = new AlphaAnimation(0, 1);
         aa.setDuration(2000);//设置间隔时间为2秒
         aa.setFillAfter(true);//动画完成之后填充原来的效果
 
@@ -195,7 +324,7 @@ public class SplashActivity extends Activity {
             versionCode = packageInfo.versionCode;
             String versionName = packageInfo.versionName;
             //显示到界面上
-            tv_title.setText(versionName+":"+ versionCode);
+            tv_title.setText(versionName);
 
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
@@ -212,5 +341,7 @@ public class SplashActivity extends Activity {
         pm = getPackageManager();
         //获取相对布局组件
         rl_root = (RelativeLayout) findViewById(R.id.rl_splash_root);
+        //获取下载的进度条
+        pb_download = (ProgressBar) findViewById(R.id.pb_splash_download);
     }
 }
